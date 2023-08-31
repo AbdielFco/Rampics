dcd256b729a1f929061320372cf08175b1c35e4a
AbdielFco <88990612+AbdielFco@users.noreply.github.com>
2023-08-31 08:41:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 const imageElement = document.getElementById("image");
-const heartElement = document.getElementById("heart");
 const nextButton = document.getElementById("nextButton");
-const rankButton = document.getElementById("rankButton");
-const homeButton = document.getElementById("homeButton");
-const galleryButton = document.getElementById("galleryButton");
-
-const images = [
-    // Array de URLs de imágenes
-];
-
-let currentIndex = 0;
-let likedImages = [];
 
 function getRandomImageURL() {
-    // Función para obtener URL aleatoria de imagen
+    const width = 300 + Math.floor(Math.random() * 300);
+    const height = 300 + Math.floor(Math.random() * 300);
+    return `https://picsum.photos/${width}/${height}`;
 }
 
 function updateImage() {
@@ -22,41 +13,13 @@ function updateImage() {
 }
 
 function showNextImage() {
-    heartElement.style.display = "none"; // Ocultar el corazón si aparece
     updateImage();
 }
 
-function showHeartAnimation() {
-    heartElement.style.display = "block";
-    setTimeout(() => {
-        heartElement.style.display = "none";
-    }, 1000);
-}
-
-function addLikedImage() {
-    if (!likedImages.includes(currentIndex)) {
-        likedImages.push(currentIndex);
-        showHeartAnimation();
-    }
-}
-
 nextButton.addEventListener("click", showNextImage);
-circle.addEventListener("dblclick", addLikedImage);
 
-rankButton.addEventListener("click", () => {
-    alert("Este es el ranking");
-});
-
-homeButton.addEventListener("click", () => {
-    // Puedes añadir aquí la lógica para la página Home
-});
-
-galleryButton.addEventListener("click", () => {
-    // Puedes añadir aquí la lógica para la página Gallery
-});
-
-// Carga inicial de imagen
+// Initial image load
 updateImage();
 
-// Cambiar imagen automáticamente cada 30 segundos
+// Auto-change image every 30 seconds
 setInterval(showNextImage, 30000);
