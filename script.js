72c85b58a9c2cbc83a4907c3565f3aa960233305
AbdielFco <88990612+AbdielFco@users.noreply.github.com>
2023-08-31 09:17:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const imageElement = document.getElementById("image");
 const nextButton = document.getElementById("nextButton");
 
+// ... (código JavaScript existente) ...
+
+const categorySelect = document.getElementById("categorySelect");
+
+function filterImages(category) {
+    if (category === "all") {
+        return images;
+    } else {
+        return images.filter(image => image.category === category);
+    }
+}
+
+categorySelect.addEventListener("change", () => {
+    const selectedCategory = categorySelect.value;
+    const filteredImages = filterImages(selectedCategory);
+    currentIndex = 0; // Resetear el índice al cambiar de categoría
+    images = filteredImages;
+    updateImage();
+});
+
+// ... (resto del código JavaScript existente) ...
+
+
 function getRandomImageURL() {
     const width = 300 + Math.floor(Math.random() * 300);
     const height = 300 + Math.floor(Math.random() * 300);
